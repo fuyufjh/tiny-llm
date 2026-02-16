111840b911cea3f206bf625f432425f2f19275dd
Eric Fu <fuyufjh@gmail.com>
2026-02-16 21:07:36 +0800

    day3 task3: impl Qwen2MultiHeadAttention

--- src/tiny_llm/qwen2_week1.py
+++ src/tiny_llm/qwen2_week1.py
@@ -24,14 +24,44 @@ class Qwen2MultiHeadAttention:
         max_seq_len: int = 32768,
         theta: int = 1000000,
     ):
-        pass
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        self.num_kv_heads = num_kv_heads # grouped
+        self.head_dim = hidden_size // num_heads
+        self.wq = wq
+        self.wk = wk
+        self.wv = wv
+        self.wo = wo
+        self.bq = bq
+        self.bk = bk
+        self.bv = bv
+        self.max_seq_len = max_seq_len
+        self.rope = RoPE(self.head_dim, max_seq_len, theta, False)
 
     def __call__(
         self,
-        x: mx.array,
+        x: mx.array, # batch_size x seq_length x hidden_size
         mask: mx.array | str | None = None,
     ) -> mx.array:
-        pass
+        batch_size, seq_length, _ = x.shape
+        
+        # Q,K,V: (batch_size, seq_length, num_(kv_)heads, head_dim)
+        q = linear(x, self.wq, self.bq).reshape(batch_size, seq_length, self.num_heads, self.head_dim)
+        k = linear(x, self.wk, self.bk).reshape(batch_size, seq_length, self.num_kv_heads, self.head_dim)
+        v = linear(x, self.wv, self.bv).reshape(batch_size, seq_length, self.num_kv_heads, self.head_dim)
+
+        q = self.rope(q, offset=slice(0, seq_length)) # batch_size x seq_length x num_heads x head_dim
+        k = self.rope(k, offset=slice(0, seq_length))
+
+        q = q.transpose(0, 2, 1, 3) # batch_size x num_heads x seq_length x head_dim
+        k = k.transpose(0, 2, 1, 3) # batch_size x num_kv_heads x seq_length x head_dim
+        v = v.transpose(0, 2, 1, 3) # batch_size x num_kv_heads x seq_length x head_dim
+
+        x = scaled_dot_product_attention_grouped(q, k, v, mask=mask) # batch_size x num_heads x seq_length x head_dim
+
+        x = x.transpose(0, 2, 1, 3).reshape(batch_size, seq_length, self.hidden_size)
+        x = linear(x, self.wo)
+        return x
 
 
 class Qwen2MLP:
