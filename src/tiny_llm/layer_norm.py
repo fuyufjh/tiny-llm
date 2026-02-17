6078a253f3b9b0c543ee474b0af5f67a288f97ed
Eric Fu <fuyufjh@gmail.com>
2026-02-17 20:58:47 +0800

    day4 task1: RMSNorm

--- src/tiny_llm/layer_norm.py
+++ src/tiny_llm/layer_norm.py
@@ -1,9 +1,13 @@
 import mlx.core as mx
 
-
+# Root Mean Square Layer Normalization
 class RMSNorm:
     def __init__(self, dim: int, weight: mx.array, eps: float = 1e-5):
-        pass
+        self.dim = dim
+        self.weight = weight
+        self.eps = eps
 
     def __call__(self, x: mx.array) -> mx.array:
-        pass
+        return (x
+                * mx.rsqrt(x.astype(mx.float32).square().mean(-1, keepdims=True) + self.eps) 
+                * self.weight).astype(x.dtype)
