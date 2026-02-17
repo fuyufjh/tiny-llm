904b384b73d5d2ad1a8e363b68e107227f4684cc
Eric Fu <fuyufjh@gmail.com>
2026-02-17 23:12:45 +0800

    day5 task2: impl embedding

--- src/tiny_llm/embedding.py
+++ src/tiny_llm/embedding.py
@@ -3,10 +3,21 @@ import mlx.core as mx
 
 class Embedding:
     def __init__(self, vocab_size: int, embedding_dim: int, weight: mx.array):
-        pass
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.weight = weight # vocab_size x embedding_dim
 
-    def __call__(self, x: mx.array) -> mx.array:
-        pass
+    def __call__(self,
+                 x: mx.array # N.. (tokens)
+                 ) -> mx.array:
+        # lookup embedding vector for each token
+        return self.weight[x, :] # N.. x embedding_dim
+        
 
-    def as_linear(self, x: mx.array) -> mx.array:
-        pass
+    def as_linear(self,
+                  x: mx.array # N.. x embedding_dim
+                  ) -> mx.array:
+        # To project the embedding back to vocab space, we can calculate the dot product (similarity) 
+        # between the input and each embedding vector.
+        # This is equivalent to a linear layer with the embedding weight as the weight matrix.
+        return x @ self.weight.T
