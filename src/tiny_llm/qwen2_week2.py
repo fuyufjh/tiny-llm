a05b1e4645253c7f059002ffeb4ad8bf6421d6a4
Eric Fu <fuyufjh@gmail.com>
2026-02-20 00:46:03 +0800

    week2 day2 task4: intergration

--- src/tiny_llm/qwen2_week2.py
+++ src/tiny_llm/qwen2_week2.py
@@ -1,4 +1,6 @@
 import mlx.core as mx
+
+from tiny_llm.quantize import quantized_linear
 from .basics import linear, silu
 from .attention import scaled_dot_product_attention_grouped
 from .layer_norm import RMSNorm
@@ -30,10 +32,10 @@ class Qwen2MultiHeadAttention:
         self.num_heads = num_heads
         self.num_kv_heads = num_kv_heads # grouped
         self.head_dim = hidden_size // num_heads
-        self.wq = dequantize_linear(wq)
-        self.wk = dequantize_linear(wk)
-        self.wv = dequantize_linear(wv)
-        self.wo = dequantize_linear(wo)
+        self.wq = wq
+        self.wk = wk
+        self.wv = wv
+        self.wo = wo
         self.bq = bq
         self.bk = bk
         self.bv = bv
@@ -51,13 +53,13 @@ class Qwen2MultiHeadAttention:
         batch_size, seq_length, _ = x.shape
         
         # Q,K,V: (batch_size, seq_length, num_(kv_)heads, head_dim)
-        q = linear(x, self.wq, self.bq).reshape(batch_size, seq_length, self.num_heads, self.head_dim)
-        k = linear(x, self.wk, self.bk).reshape(batch_size, seq_length, self.num_kv_heads, self.head_dim)
-        v = linear(x, self.wv, self.bv).reshape(batch_size, seq_length, self.num_kv_heads, self.head_dim)
+        q = quantized_linear(x, self.wq, self.bq).reshape(batch_size, seq_length, self.num_heads, self.head_dim)
+        k = quantized_linear(x, self.wk, self.bk).reshape(batch_size, seq_length, self.num_kv_heads, self.head_dim)
+        v = quantized_linear(x, self.wv, self.bv).reshape(batch_size, seq_length, self.num_kv_heads, self.head_dim)
 
         offset_slices = [slice(offset, offset + seq_length) for offset in offsets]
-        q = self.rope(q, offset=offset_slices) # batch_size x seq_length x num_heads x head_dim
-        k = self.rope(k, offset=offset_slices)
+        q = self.rope(q, offset=offset_slices).astype(mx.float16) # batch_size x seq_length x num_heads x head_dim
+        k = self.rope(k, offset=offset_slices).astype(mx.float16)
 
         # Update the cache and fetch the updated full key and value
         k, v, _, mask = cache.update_and_fetch(k, v, mask_length=seq_length, mask=mask)
@@ -69,7 +71,7 @@ class Qwen2MultiHeadAttention:
         x = scaled_dot_product_attention_grouped(q, k, v, mask=mask) # batch_size x num_heads x seq_length x head_dim
 
         x = x.transpose(0, 2, 1, 3).reshape(batch_size, seq_length, self.hidden_size)
-        x = linear(x, self.wo)
+        x = quantized_linear(x, self.wo)
         return x
 
 
@@ -84,14 +86,14 @@ class Qwen2MLP:
     ):
         self.dim = dim
         self.hidden_dim = hidden_dim
-        self.w_gate = dequantize_linear(w_gate)
-        self.w_up = dequantize_linear(w_up)
-        self.w_down = dequantize_linear(w_down)
+        self.w_gate = w_gate
+        self.w_up = w_up
+        self.w_down = w_down
 
     def __call__(self, x: mx.array) -> mx.array:
-        activation = silu(linear(x, self.w_gate))
-        up = linear(x, self.w_up)
-        return linear(activation * up, self.w_down)
+        activation = silu(quantized_linear(x, self.w_gate))
+        up = quantized_linear(x, self.w_up)
+        return quantized_linear(activation * up, self.w_down)
 
 
 class Qwen2TransformerBlock:
@@ -185,7 +187,7 @@ class Qwen2ModelWeek2:
         self.embedding = Embedding(
             vocab_size=args.vocab_size,
             embedding_dim=args.hidden_size,
-            weight=dequantize_linear(model.embed_tokens),
+            weight=dequantize_linear(model.embed_tokens).astype(mx.float16), # TODO: use quantized?
         )
 
         self.blocks = []
@@ -218,7 +220,7 @@ class Qwen2ModelWeek2:
         if args.tie_word_embeddings:
             self.lm_head = None
         else:
-            self.lm_head = dequantize_linear(mlx_model.lm_head)
+            self.lm_head = mlx_model.lm_head
 
     def __call__(
         self,
@@ -231,7 +233,7 @@ class Qwen2ModelWeek2:
             x = self.blocks[i](x, offset, cache[i], mask="causal")
         x = self.rms_norm(x)
         if self.lm_head is not None:
-            x = linear(x, self.lm_head)
+            x = quantized_linear(x, self.lm_head)
         else:
             x = self.embedding.as_linear(x)
         return x
